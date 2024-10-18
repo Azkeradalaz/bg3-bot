317d5368b00b6587d2c16790720392a52b0a5513
Azkeradalaz <daazwaa@gmail.com>
2024-10-18 13:06:55 +0300

    initial

---
+++ src/test/java/ru/baldursgate3/tgbot/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.baldursgate3.tgbot.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
