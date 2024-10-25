c81636bbe6758d289bf136f68b82bcbb4d8fb549
Azkeradalaz <daazwaa@gmail.com>
2024-10-25 17:49:46 +0300

    GameCharacter entity added

--- src/main/java/ru/baldursgate3/tgbot/bot/services/RestTemplateService.java
+++ src/main/java/ru/baldursgate3/tgbot/bot/services/RestTemplateService.java
@@ -1,9 +1,6 @@
 package ru.baldursgate3.tgbot.bot.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.baldursgate3.tgbot.bot.Constant;
@@ -18,20 +15,22 @@ public class RestTemplateService {
     private final RestTemplate restTemplate;
 
     public String getUser(Long tgId) {
-        String url = Constant.HOST +"/user";
-        Map<String,String> request = new HashMap<>();
+        String url = Constant.HOST + "/user";
+        Map<String, String> request = new HashMap<>();
         request.put("tgUserId", tgId.toString());
         return restTemplate.getForObject(url, String.class, request);
     }
+
     public String getUserByTgId(Long tgId) {
-        String url = Constant.HOST +"/user/tgid/"+tgId;
-        return restTemplate.getForObject(url,String.class);
+        String url = Constant.HOST + "/user/tgid/" + tgId;
+        return restTemplate.getForObject(url, String.class);
     }
-    public String registerUser(Long id, String name){
-        String url = Constant.HOST +"/user";
+
+    public String registerUser(Long id, String name) {
+        String url = Constant.HOST + "/user";
         User user = new User();
         user.setTgUserId(id);
         user.setName(name);
-        return restTemplate.postForObject(url,user,String.class);
+        return restTemplate.postForObject(url, user, String.class);
     }
 }
