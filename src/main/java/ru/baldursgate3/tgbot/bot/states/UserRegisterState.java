aadf75c27556eb55813fdeb1b72e0e86bb65cac7
Azkeradalaz <daazwaa@gmail.com>
2024-11-15 22:06:18 +0300

    регистрация

--- src/main/java/ru/baldursgate3/tgbot/bot/states/UserRegisterState.java
+++ src/main/java/ru/baldursgate3/tgbot/bot/states/UserRegisterState.java
@@ -2,19 +2,41 @@ package ru.baldursgate3.tgbot.bot.states;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.baldursgate3.tgbot.bot.event.EditMessageTextEvent;
+import ru.baldursgate3.tgbot.bot.event.SendMessageEvent;
+import ru.baldursgate3.tgbot.bot.services.MessageService;
 import ru.baldursgate3.tgbot.bot.services.UserService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserRegisterState implements UserSessionState{
     private final UserService userService;
+    private final MessageService messageService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    private Set<Long> toRegister = new HashSet<>();
 
     @Override
     public void consumeMessage(Update update) {
 
+        Long userId = update.getMessage().getFrom().getId();
+        Long chatId = update.getMessage().getChatId();
+        if (toRegister.contains(userId)){
+            toRegister.remove(userId);
+            userService.registerUser(userId,update.getMessage().getText());
+            applicationEventPublisher.publishEvent(new EditMessageTextEvent(this, messageService.backToMainMenuMessage(chatId,update.getMessage().getText(), messageService.getEditMessage(chatId))));
+        }
+        else {
+            applicationEventPublisher.publishEvent(new SendMessageEvent(this,messageService.greetingNonRegisteredUser(chatId)));
+            toRegister.add(userId);
+        }
     }
 
     @Override
